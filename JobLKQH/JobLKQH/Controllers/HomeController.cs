ada8bfffba71708999816ccb20f967436f2d4a62
Huy Tran <tranquochuy.d22ctc1@muce.edu.vn>
2025-11-27 15:17:45 +0700

    Login_commit

--- JobLKQH/JobLKQH/Controllers/HomeController.cs
+++ JobLKQH/JobLKQH/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace JobLKQH.Controllers
         {
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
