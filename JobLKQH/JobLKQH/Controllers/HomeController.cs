f9d2c4f341c464c7c7c85236f47d5390950c9a0d
Huy Tran <tranquochuy.d22ctc1@muce.edu.vn>
2025-11-27 17:24:25 +0700

    SearchWork_commit

--- JobLKQH/JobLKQH/Controllers/HomeController.cs
+++ JobLKQH/JobLKQH/Controllers/HomeController.cs
@@ -30,6 +30,10 @@ namespace JobLKQH.Controllers
         {
             return View();
         }
+        public IActionResult SearchWork()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
