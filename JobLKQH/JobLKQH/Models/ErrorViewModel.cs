29db49967bcc62dfe7b1bca7bd341f53d6cc96e7
NguyenLuuKienn <nguyenluukien.d23ctc4@muce.edu.vn>
2025-11-27 13:35:33 +0700

    first_commit

---
+++ JobLKQH/JobLKQH/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobLKQH.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
